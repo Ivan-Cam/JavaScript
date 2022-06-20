7c8d5c82c13903d612429a6bba6f6105c882a3c0
Ivan Campos <ivan820plus@gmail.com>
2022-06-20 17:01:17 -0500

    added objects file

--- apps/objetos.js
+++ apps/objetos.js
@@ -1,1 +1,32 @@
-console.log('first');
+const IVA_GENERAL = 1.21;
+const IVA_REDUCIDO = 1.1;
+
+const item1 = {
+	precio: 15,
+	cantidad: 2,
+	impuestos: IVA_GENERAL,
+	calcularTotal: function () {
+		return this.cantidad * this.precio * this.impuestos;
+	},
+};
+
+const item2 = {
+	precio: 25,
+	cantidad: 3,
+	impuestos: IVA_REDUCIDO,
+	calcularTotal: function () {
+		return this.cantidad * this.precio * this.impuestos;
+	},
+};
+
+const factura = {
+	item1,
+	item2,
+	calcularTotal: function (descuento) {
+		return (
+			(this.item1.calcularTotal() + this.item2.calcularTotal()) * descuento
+		);
+	},
+};
+
+console.log(factura.calcularTotal(0.8));
